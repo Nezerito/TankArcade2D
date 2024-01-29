26a40e1b4b8eafe97db9546c1de0dec17cd18be4
Nezerito <Nezerito@yandex.ru>
2024-01-29 23:19:50 +0300

    Комментирование

--- code.py
+++ code.py
@@ -5,7 +5,7 @@ from pygame.math import Vector2
 from random import randint
 
 if __name__ == '__main__':
-    pygame.init()
+    pygame.init()  # создание окна
     pygame.display.set_caption('TankArcade2D')
     size = width, height = 800, 600
     screen = pygame.display.set_mode(size)
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     pygame.display.set_icon(icon)
     pygame.mixer.init()
 
-    fontUI = pygame.font.Font(None, 30)
+    fontUI = pygame.font.Font(None, 30)  # добавление изображений и шрифтов
     tank_image = pygame.image.load('images/tank.png')
     bullet_image = pygame.image.load('images/bullet.png')
     brick_image = pygame.image.load('images/block_brick.png')
@@ -34,41 +34,40 @@ if __name__ == '__main__':
     TILE = 32
 
 
-    def terminate():
+    def terminate():  # выход из игры
         pygame.quit()
         sys.exit()
 
 
-    def text_objects(text, color, text_size="small"):
+    def text_objects(text, color, text_size="small"):  # размеры шрифтов
         text_surface = small_font.render(text, True, color)
         if text_size == "medium":
             text_surface = medium_font.render(text, True, color)
         if text_size == "large":
             text_surface = large_font.render(text, True, color)
-        if text_size == "vsmall":
+        if text_size == "smallest":
             text_surface = smallest_font.render(text, True, color)
 
         return text_surface, text_surface.get_rect()
 
 
-    def message_to_screen(msg, color, y_displace=0, mes_size="small"):
+    def message_to_screen(msg, color, y_displace=0, mes_size="small"):  # надпись на экране
         text_surf, text_rect = text_objects(msg, color, mes_size)
         text_rect.center = (int(width / 2), int(height / 2) + y_displace)
         screen.blit(text_surf, text_rect)
 
 
-    def text_to_button(msg, color, btn_x, btn_y, btn_width, btn_height, btn_size="vsmall"):
+    def text_to_button(msg, color, btn_x, btn_y, btn_width, btn_height, btn_size="smallest"):  # текст кнопки
         text_surf, text_rect = text_objects(msg, color, btn_size)
         text_rect.center = ((btn_x + (btn_width / 2)), btn_y + (btn_height / 2))
         screen.blit(text_surf, text_rect)
 
 
-    def button(text, x, y, btn_width, btn_height, inactive_color, active_color, action=None):
+    def button(text, px, py, btn_width, btn_height, inactive_color, active_color, action=None):  # создание кнопки
         cur = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
-        # print(click)
-        if x + btn_width > cur[0] > x and y + btn_height > cur[1] > y:
-            pygame.draw.rect(screen, active_color, (x, y, btn_width, btn_height))
+        if px + btn_width > cur[0] > px and py + btn_height > cur[1] > py:
+            pygame.draw.rect(screen, active_color, (px, py, btn_width, btn_height))  # подсветка при наводке
             if click[0] == 1 and action is not None:
                 if action == "quit":
                     terminate()
@@ -83,12 +82,12 @@ if __name__ == '__main__':
                     return True
 
         else:
-            pygame.draw.rect(screen, inactive_color, (x, y, btn_width, btn_height))
+            pygame.draw.rect(screen, inactive_color, (px, py, btn_width, btn_height))
 
-        text_to_button(text, 'black', x, y, btn_width, btn_height)
+        text_to_button(text, 'black', px, py, btn_width, btn_height)
 
 
-    def game_intro():
+    def game_intro():  # начальное окно
         while True:
             screen.fill('black')
             message_to_screen("Welcome to TankArcade2D!", 'red', -100, mes_size="large")
@@ -111,7 +110,7 @@ if __name__ == '__main__':
             clock.tick(FPS)
 
 
-    def pause():
+    def pause():  # пауза
         paused = True
         message_to_screen("Paused", 'white', -100, mes_size="large")
         message_to_screen("Press Esc to continue playing or Q to quit", 'green', 25)
@@ -127,13 +126,12 @@ if __name__ == '__main__':
                         terminate()
 
 
-    def game_over(player):
+    def game_over(player):  # финальное окно
         pygame.mixer.music.load('lose_music.mp3')
         pygame.mixer.music.play()
         end_time = time.time()
-        with open('data.txt', mode='a+') as file:
-            file.write(f'Winner: {player}, time: {int(end_time - start_time)}, '
-                       f'shots: {bullet_count}\n')
+        with open('data.txt', mode='a+') as file:  # запись итогов
+            file.write(f'Winner: {player}, time: {int(end_time - start_time)}, shots: {bullet_count}\n')
 
         while True:
             screen.fill('black')
@@ -159,7 +157,7 @@ if __name__ == '__main__':
             clock.tick(FPS)
 
 
-    def game_controls():
+    def game_controls():  # клавиши управления
         while True:
             screen.fill('black')
             message_to_screen("Controls", 'yellow', -100, mes_size="large")
@@ -184,7 +182,7 @@ if __name__ == '__main__':
             clock.tick(FPS)
 
 
-    class Tank:
+    class Tank:  # создание танка
         def __init__(self, color, px, py, angle, vector):
             self.color = color
             pos = (px, py)
@@ -193,14 +191,14 @@ if __name__ == '__main__':
             self.image = pygame.transform.rotate(tank_image, -angle)
             self.original_image = self.image
 
-            self.rect = self.image.get_rect(center=pos)
+            self.rect = self.image.get_rect(center=pos)  # переменные передвижения
             self.position = Vector2(pos)
             self.direction = Vector2(vector, 0)
             self.speed = 0
             self.angle_speed = 0
             self.angle = 0
 
-            self.hp = 1
+            self.hp = 5  # переменные жизни и выстрелов
             self.shotTimer = 0
             self.shot = False
             self.shotDelay = 60
@@ -208,21 +206,19 @@ if __name__ == '__main__':
             self.bulletDamage = 1
 
         def update(self):
-            if self.angle_speed != 0:
-                # Rotate the direction vector and then the image.
+            if self.angle_speed != 0:  # вращение танка
                 self.direction.rotate_ip(self.angle_speed)
                 self.angle += self.angle_speed
                 self.image = pygame.transform.rotate(self.original_image, -self.angle)
                 self.rect = self.image.get_rect(center=self.rect.center)
-            # Update the position vector and the rect.
             self.position += self.direction * self.speed
             self.rect.center = self.position
-            for obj in objects:
-                if obj != self and obj.type in 'block' and self.rect.colliderect(obj.rect):
+            for sprite in objects:  # создание коллизии
+                if sprite != self and sprite.type in 'block' and self.rect.colliderect(sprite.rect):
                     self.position += -self.direction * self.speed
                     self.rect.center = self.position
 
-            if self.shot and self.shotTimer == 0:
+            if self.shot and self.shotTimer == 0:  # выстрел
                 global bullet_count
                 bullet_count += 1
                 new_position = self.position + self.direction * 10
@@ -230,7 +226,7 @@ if __name__ == '__main__':
                 self.shotTimer = self.shotDelay
                 self.shot = False
 
-            if self.shotTimer > 0:
+            if self.shotTimer > 0:  # перезарядка
                 self.shotTimer -= 1
 
         def draw(self):
@@ -242,7 +238,7 @@ if __name__ == '__main__':
                 objects.remove(self)
 
 
-    class Bullet:
+    class Bullet:  # создание патрона
         def __init__(self, parent, pos, direction, damage, speed, angle):
             bullets.append(self)
             self.image = pygame.transform.rotate(bullet_image, -angle - 90)
@@ -260,9 +256,9 @@ if __name__ == '__main__':
             if self.position[0] < 0 or self.position[0] > width or self.position[1] < 0 or self.position[1] > height:
                 bullets.remove(self)
             else:
-                for obj in objects:
-                    if obj != self.parent and obj.type != 'bang' and obj.rect.collidepoint(self.position):
-                        obj.damage(self.damage)
+                for sprite in objects:
+                    if sprite != self.parent and sprite.type != 'bang' and sprite.rect.collidepoint(self.position):
+                        sprite.damage(self.damage)
                         bullets.remove(self)
                         Bang(self.position)
                         break
@@ -271,12 +267,12 @@ if __name__ == '__main__':
             screen.blit(self.image, self.rect)
 
 
-    class Block:
-        def __init__(self, px, py, size):
+    class Block:  # создание блока
+        def __init__(self, px, py, sizes):
             objects.append(self)
             self.type = 'block'
 
-            self.rect = pygame.Rect(px, py, size, size)
+            self.rect = pygame.Rect(px, py, sizes, sizes)
             self.hp = 1
 
         def update(self):
@@ -291,7 +287,7 @@ if __name__ == '__main__':
                 objects.remove(self)
 
 
-    class UI:
+    class UI:  # отображение интерфейса жизней
         def __init__(self):
             pass
 
@@ -300,17 +296,17 @@ if __name__ == '__main__':
 
         def draw(self):
             i = 0
-            for obj in objects:
-                if obj.type == 'tank':
-                    pygame.draw.rect(screen, obj.color, (5 + i * 70, 5, 22, 22))
+            for sprite in objects:
+                if sprite.type == 'tank':
+                    pygame.draw.rect(screen, sprite.color, (5 + i * 70, 5, 22, 22))
 
-                    text = fontUI.render(str(obj.hp), 1, obj.color)
-                    rect = text.get_rect(center=(5 + i * 70 + 32, 5 + 11))
-                    screen.blit(text, rect)
+                    text = fontUI.render(str(sprite.hp), 1, sprite.color)
+                    rects = text.get_rect(center=(5 + i * 70 + 32, 5 + 11))
+                    screen.blit(text, rects)
                     i += 1
 
 
-    class Bang:
+    class Bang:  # взрыв снаряда
         def __init__(self, pos):
             objects.append(self)
             self.type = 'bang'
@@ -319,14 +315,14 @@ if __name__ == '__main__':
             self.frame = 0
 
         def update(self):
-            self.frame += 0.2
+            self.frame += 0.2  # анимация взрыва
             if self.frame >= 3:
                 objects.remove(self)
 
         def draw(self):
             image = bangs_images[int(self.frame)]
-            rect = image.get_rect(center=self.position)
-            screen.blit(image, rect)
+            rects = image.get_rect(center=self.position)
+            screen.blit(image, rects)
 
 
     bullet_count = 0
@@ -335,7 +331,7 @@ if __name__ == '__main__':
     player1 = Tank('blue', 100, 275, 90, 1)
     player2 = Tank('red', 650, 275, -90, -1)
     ui = UI()
-    for _ in range(50):
+    for _ in range(50):  # создание карты
         while True:
             x = randint(0, width // TILE - 1) * TILE
             y = randint(0, height // TILE - 1) * TILE
@@ -427,12 +423,13 @@ if __name__ == '__main__':
                 if event.key == pygame.K_ESCAPE:
                     pause()
         screen.fill('black')
+        # изменение объектов
         for obj in objects:
             obj.update()
         for bullet in bullets:
             bullet.update()
         ui.update()
-
+        # отрисовка объектов
         for obj in objects:
             obj.draw()
         for bullet in bullets:
@@ -445,5 +442,6 @@ if __name__ == '__main__':
         elif player2.hp <= 0:
             game_over('1 player')
             start = True
+
         pygame.display.update()
         clock.tick(FPS)
